650a1f4f63603938a6a9dfbf0d390323d792aebe
Alex Bondar <abondar1992@gmail.com>
2024-04-01 23:44:16 +0200

    stat controller full

--- Webhook/ChatAnalyzerBot/src/main/kotlin/org/abondar/experimental/telegrambots/stats/StatsController.kt
+++ Webhook/ChatAnalyzerBot/src/main/kotlin/org/abondar/experimental/telegrambots/stats/StatsController.kt
@@ -2,21 +2,26 @@ package org.abondar.experimental.telegrambots.stats
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.PathVariable
+import io.micronaut.serde.ObjectMapper
+import org.abondar.experimental.telegrambots.counter.WordCountService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 @Controller("/stats")
 class StatsController (
-  //TODO: add redis service
+    private val wordCountService: WordCountService,
+    private val objectMapper: ObjectMapper
 ){
 
     private val logger: Logger = LoggerFactory.getLogger(StatsController::class.java)
 
-    @Get
-    fun statistics() {
-        //TODO: add offset and linit
+    @Get("/{limit}")
+    fun statistics(@PathVariable limit: Int): String {
         logger.info("Fetching statistics")
 
+        val wordStat = wordCountService.getWordStat(limit)
 
+        return objectMapper.writeValueAsString(wordStat)
     }
 }
